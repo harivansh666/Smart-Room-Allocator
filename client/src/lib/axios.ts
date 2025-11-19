9d6513a7dec0bf8525bd42c5576fc38a1b44b3ef
Harivansh <jazzz1815@gmail.com>
2025-11-19 18:28:22 +0530

    done

--- client/src/lib/axios.ts
+++ client/src/lib/axios.ts
@@ -1,6 +1,7 @@
 import axios from "axios"
 export const axiosInstance = axios.create({
-    // baseURL: import.meta.env.VITE_API_URL,
-    baseURL: "https://smart-room-allocator.vercel.app/api",
+    baseURL: "http://localhost:3000/api",
     withCredentials: true,
+    // baseURL: import.meta.env.VITE_API_URL,
+    // baseURL: "https://smart-room-allocator.vercel.app/api",
 });
