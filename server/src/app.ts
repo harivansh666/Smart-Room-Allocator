728b173880fa4cf5b8c41e97c19f53622f96cd7a
Harivansh <jazzz1815@gmail.com>
2025-11-19 15:43:32 +0530

    check

--- server/src/app.ts
+++ server/src/app.ts
@@ -13,7 +13,7 @@ app.use(express.json())
 app.use(cookieParser())
 
 app.use(cors({
-    origin: "https://smart-room-allocator.vercel.app",
+    origin: "https://shiny-melomakarona-5f5c6d.netlify.app",
     methods: ["GET,POST,PUT,DELETE,PATCH"],
     credentials: true
 }))
